dbdaeecd5c919a658f8528504f93607fdc6fc35f
y-eren <84980503+y-eren@users.noreply.github.com>
2024-09-03 21:38:45 +0300

    spring-hibernate-jpa

---
+++ spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/luv2code/cruddemo/dao/StudentDAO.java
@@ -0,0 +1,22 @@
+package com.luv2code.cruddemo.dao;
+
+import java.util.List;
+
+import com.luv2code.cruddemo.entity.Student;
+
+public interface StudentDAO {
+
+	void save(Student theStudent);
+	Student findById(Integer id);
+	
+	List<Student> findAll();
+	
+	List<Student>findByLastName(String lastName);
+	
+	void update(Student theStudent);
+	
+	void delete(Integer id);
+	
+	int deleteAll();
+	
+}
