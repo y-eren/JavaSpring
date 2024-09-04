c11855370a262e8b83d17090a56d43f04eea0386
y-eren <84980503+y-eren@users.noreply.github.com>
2024-09-04 21:05:47 +0300

    spring-boot-rest-crud

---
+++ spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/luv2code/demo/rest/StudentRestController.java
@@ -0,0 +1,62 @@
+package com.luv2code.demo.rest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.luv2code.demo.entity.Student;
+
+import jakarta.annotation.PostConstruct;
+
+@RestController
+@RequestMapping("/api")
+public class StudentRestController {
+	
+	private List<Student> theStudents;
+	
+	@PostConstruct
+	public void loadData()
+	{
+		theStudents = new ArrayList<>();
+		
+		theStudents.add(new Student("Poornima", "Patel"));
+		theStudents.add(new Student("Mario", "Rossi"));
+		theStudents.add(new Student("Mary", "Smith"));
+	}
+
+	//define endpoint for "/students"  - return a list of students
+
+	@GetMapping("/students")
+	public List<Student> getStudents()
+	{
+		
+		return theStudents;
+	}
+	
+	//define endpoint or "/students/{studenId}" - return student at index
+	
+	@GetMapping("/students/{studentId}")
+	public Student getStudent(@PathVariable int studentId) {
+		// check the studentId again list size
+		if((studentId >= theStudents.size()) || (studentId < 0))
+		{
+			throw new StudentNotFoundException("Student id not found - "+ studentId);
+		}
+		return theStudents.get(studentId);
+		
+	}
+	
+	
+	// Add an exception handler using @ExpeptionHandler
+	
+	
+	
+	
+}
---
+++ spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/luv2code/demo/rest/StudentRestExceptionHandler.java
@@ -0,0 +1,39 @@
+package com.luv2code.demo.rest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class StudentRestExceptionHandler {
+ // add exception handle code here
+	
+	@ExceptionHandler
+	public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exc) {
+		// create a StudentErrorResponse
+		StudentErrorResponse error = new StudentErrorResponse();
+		// return ResponseEntity
+		error.setStatus(HttpStatus.NOT_FOUND.value());
+		error.setMessage(exc.getMessage());
+		error.setTimeStamp(System.currentTimeMillis());;
+		return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+	}
+	
+	
+	// add another exception handler.. to catch any exception (catch all)
+	
+	@ExceptionHandler
+	public ResponseEntity<StudentErrorResponse> handleException(Exception exc)
+	{
+		// create a StudentErrorResponse
+		StudentErrorResponse error = new StudentErrorResponse();
+		// return ResponseEntity
+		error.setStatus(HttpStatus.BAD_REQUEST.value());
+		error.setMessage(exc.getMessage()); // burayı değiştirerek istediğimiz mesajı yazabilmekteyiz.
+		error.setTimeStamp(System.currentTimeMillis());;
+		return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+	}
+	
+	
+}
