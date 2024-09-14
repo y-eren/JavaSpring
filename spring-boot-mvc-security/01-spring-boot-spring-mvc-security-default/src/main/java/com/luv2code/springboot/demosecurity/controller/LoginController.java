aad742b4b8ecf5e8cbd442c1e1b92fa48079a85a
y-eren <84980503+y-eren@users.noreply.github.com>
2024-09-14 21:15:25 +0300

    spring boot mvc security

---
+++ spring-boot-mvc-security/01-spring-boot-spring-mvc-security-default/src/main/java/com/luv2code/springboot/demosecurity/controller/LoginController.java
@@ -0,0 +1,23 @@
+package com.luv2code.springboot.demosecurity.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+	 @GetMapping("/showLoginPage")
+	 public String showLoginPage()
+	 {
+		 return "plain-login";
+	 }
+	 
+	 @GetMapping("/access-denied")
+	 public String showAccessDenied()
+	 {
+		 return "access-denied";
+	 }
+	 
+	  
+	 
+}
